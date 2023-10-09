c5e32203216d40a59b7bfdbc96a8d29d8695ea72
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-09 19:36:26 +0300

    logged event and used some event object methods like target

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,39 +1,22 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
-// button.onclick = function () {}; // this always should be set equal to a function
-
-const buttonClickHandler = () => {
-  alert('Button was clicked!');
+const buttonClickHandler = event => {
+  event.target.disabled = true;
+  console.log(event);
 };
 
 const anotherButtonClickHandler = () => {
   console.log('This was clicked!');
 };
 
-// button.onclick = buttonClickHandler;
-// button.onclick = anotherButtonClickHandler; // this overrides the old listener
+const bounFn = buttonClickHandler.bind(this);
 
-button.addEventListener('click', buttonClickHandler);
-
-setTimeout(() => {
-  button.removeEventListener('click', buttonClickHandler);
-}, 2000);
-
-// button.addEventListener('click', () => {
-//   console.log('Clicked!');
-// });
+// button.addEventListener('click', buttonClickHandler);
 
 // setTimeout(() => {
-//   button.removeEventListener('click', () => {
-//     console.log('Clicked!');
-//   });
+//   button.removeEventListener('click', buttonClickHandler);
 // }, 2000);
-// => This way is not working
 
-const bounFn = buttonClickHandler.bind(this); // you will have to store the bound function in a new constant for removeEventListener to work
-
-button.addEventListener('click', bounFn);
-
-setTimeout(() => {
-  button.removeEventListener('click', bounFn);
-}, 2000);
+buttons.forEach(btn => {
+  btn.addEventListener('click', buttonClickHandler);
+});
