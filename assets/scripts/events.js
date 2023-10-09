bcab59d8edeefdf9bf8deb1de60b7b3cc3076481
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-09 18:15:58 +0300

    introduced the different ways of listening to events

---
+++ assets/scripts/events.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button');
+
+// button.onclick = function () {}; // this always should be set equal to a function
+
+// const buttonClickHandler = () => {
+//   alert('Button was clicked!');
+// };
+
+// const anotherButtonClickHandler = () => {
+//   console.log('This was clicked!');
+// };
+
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickHandler; // this overrides the old listener
+// // it has one major downside, we can only add one handler for this kind of event to this element.
+
+button.addEventListener();
+
+button.removeEventListener();
