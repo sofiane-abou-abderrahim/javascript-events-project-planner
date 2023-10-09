602a4e1054ea6afcb04fc71c4fa05592d18697d6
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-09 19:08:11 +0300

    different ways of removing event listeners

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,18 +2,38 @@ const button = document.querySelector('button');
 
 // button.onclick = function () {}; // this always should be set equal to a function
 
-// const buttonClickHandler = () => {
-//   alert('Button was clicked!');
-// };
+const buttonClickHandler = () => {
+  alert('Button was clicked!');
+};
 
-// const anotherButtonClickHandler = () => {
-//   console.log('This was clicked!');
-// };
+const anotherButtonClickHandler = () => {
+  console.log('This was clicked!');
+};
 
 // button.onclick = buttonClickHandler;
 // button.onclick = anotherButtonClickHandler; // this overrides the old listener
-// // it has one major downside, we can only add one handler for this kind of event to this element.
 
-button.addEventListener();
+button.addEventListener('click', buttonClickHandler);
 
-button.removeEventListener();
+setTimeout(() => {
+  button.removeEventListener('click', buttonClickHandler);
+}, 2000);
+
+// button.addEventListener('click', () => {
+//   console.log('Clicked!');
+// });
+
+// setTimeout(() => {
+//   button.removeEventListener('click', () => {
+//     console.log('Clicked!');
+//   });
+// }, 2000);
+// => This way is not working
+
+const bounFn = buttonClickHandler.bind(this); // you will have to store the bound function in a new constant for removeEventListener to work
+
+button.addEventListener('click', bounFn);
+
+setTimeout(() => {
+  button.removeEventListener('click', bounFn);
+}, 2000);
